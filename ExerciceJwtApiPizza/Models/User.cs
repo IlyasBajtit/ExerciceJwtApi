0561a2a9a0a6ac0ea57a0a6d2cab37a2ee45926b
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-09 12:22:01 +0100

    feat: add model user

--- ExerciceJwtApiPizza/Models/User.cs
+++ ExerciceJwtApiPizza/Models/User.cs
@@ -1,6 +1,32 @@
-﻿namespace ExerciceJwtApiPizza.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExerciceJwtApiPizza.Models
 {
     public class User
     {
+
+        [Column("Id")]
+        public int Id { get; set; } 
+
+        [Column("FirstName")]
+        public string FirstName { get; set; } 
+
+        [Column("LastName")]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"^([a-zA-Z0-9\.\-_]+)@([a-zA-Z0-9\-_]+)(\.)?([a-zA-Z0-9\-_]+)?(\.){1}([a-zA-Z]{2,11})$", ErrorMessage = "Invalid email address")]
+        [Required]
+        public String Email { get; set; }
+
+        [Column("PhoneNumber")]
+        public string PhoneNumber { get; set; } 
+
+        [Column("Address")]
+        public string Address { get; set; }
+
+        public bool IsAdmin { get; set; } = false;
+
+
     }
 }
