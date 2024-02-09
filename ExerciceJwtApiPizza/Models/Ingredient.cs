c3b6e6947ed5b79433d8b786db3ab2cc23a0cdb2
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-09 14:44:55 +0100

    feat: add Model Ingredient

---
+++ ExerciceJwtApiPizza/Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExerciceJwtApiPizza.Models
+{
+    public class Ingredient
+    {
+        [Column("Id")]
+        public int Id { get; set; } 
+
+        [Column("Name")]
+        public string Name { get; set; } 
+
+        [Column("Description")]
+        public string Description { get; set; } 
+    }
+}
