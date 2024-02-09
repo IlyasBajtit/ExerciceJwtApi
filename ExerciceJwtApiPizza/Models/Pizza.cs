0b7f52f5d0b4684718e21895ba5d6f09c4542e7c
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-09 13:41:28 +0100

    feat: add Pizza Models

--- ExerciceJwtApiPizza/Models/Pizza.cs
+++ ExerciceJwtApiPizza/Models/Pizza.cs
@@ -1,6 +1,31 @@
-﻿namespace ExerciceJwtApiPizza.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExerciceJwtApiPizza.Models
 {
     public class Pizza
     {
+        [Column("Id")]
+        public int Id { get; set; } 
+
+        [Column("Name")]
+        public string Name { get; set; } 
+
+        [Column("Description")]
+        public string Description { get; set; } 
+
+        [Column("Price")]
+        public decimal Price { get; set; } 
+
+        [Column("Image")]
+        public string Image { get; set; } 
+
+        [Column("Status")]
+        public PizzaStatus Status { get; set; }  
     }
+}
+public enum PizzaStatus
+{
+    Regular,
+    Vegetarian,
+    Spicy
 }
