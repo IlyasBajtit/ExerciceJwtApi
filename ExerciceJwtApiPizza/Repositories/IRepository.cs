84c30a348d70c26c480061e97abba1c9d6323a46
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-09 14:45:59 +0100

    feat: add IRepository

---
+++ ExerciceJwtApiPizza/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace ExerciceJwtApiPizza.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+
+        Task<TEntity?> Add(TEntity entity);
+
+
+        Task<TEntity?> Get(int id);
+        Task<TEntity?> Get(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate);
+
+
+        Task<TEntity?> Update(TEntity entity);
+
+
+        Task<bool> Delete(int id);
+
+    }
+}
