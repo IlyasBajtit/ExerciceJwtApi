30dfda4ca3c30a0bc6fee7e0e8e2e256ad366856
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-11 23:12:12 +0100

    feat: add LoginRequestDTO

---
+++ ExerciceJwtApiPizza/Dtos/LoginRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using ExerciceJwtApiPizza.Validator;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExerciceJwtApiPizza.Dtos
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        [PasswordValidator]
+        public string Password { get; set; }
+
+
+    }
+}
