8ee71336beb4786dace80b995e94dc0f55791038
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-11 23:13:46 +0100

    feat: update Pizza model

--- ExerciceJwtApiPizza/Models/Pizza.cs
+++ ExerciceJwtApiPizza/Models/Pizza.cs
@@ -1,31 +1,38 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExerciceJwtApiPizza.Models
 {
     public class Pizza
     {
         [Column("Id")]
-        public int Id { get; set; } 
+        public int Id { get; set; }
 
         [Column("Name")]
-        public string Name { get; set; } 
+        [Required]
+        public string Name { get; set; }
 
         [Column("Description")]
-        public string Description { get; set; } 
+        [Required]
+        public string Description { get; set; }
 
         [Column("Price")]
-        public decimal Price { get; set; } 
+        [Required]
+        public decimal Price { get; set; }
 
         [Column("Image")]
-        public string Image { get; set; } 
+        [Required]
+        public string Image { get; set; }
 
         [Column("Status")]
-        public PizzaStatus Status { get; set; }  
+        [Required]
+        public PizzaStatus Status { get; set; }
     }
-}
-public enum PizzaStatus
-{
-    Regular,
-    Vegetarian,
-    Spicy
+
+    public enum PizzaStatus
+    {
+        Regular,
+        Vegetarian,
+        Spicy
+    }
 }
