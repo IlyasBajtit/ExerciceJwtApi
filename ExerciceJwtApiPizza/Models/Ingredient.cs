84ebb6fe3a89e13c103f9945fc05b6abe018306f
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-11 23:13:37 +0100

    feat: update Pizza model

--- ExerciceJwtApiPizza/Models/Ingredient.cs
+++ ExerciceJwtApiPizza/Models/Ingredient.cs
@@ -1,16 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExerciceJwtApiPizza.Models
 {
+    [Table("ingredient")]
     public class Ingredient
     {
-        [Column("Id")]
-        public int Id { get; set; } 
-
-        [Column("Name")]
-        public string Name { get; set; } 
-
-        [Column("Description")]
-        public string Description { get; set; } 
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; }
+        [Column("description")]
+        [Required]
+        public string Description { get; set; }
     }
 }
