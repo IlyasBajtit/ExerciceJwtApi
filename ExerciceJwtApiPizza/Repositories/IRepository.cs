f4560491b1213ba8dfabeef1c1dd129fc7371d68
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-11 21:23:47 +0100

    feat: Update IRepository

--- ExerciceJwtApiPizza/Repositories/IRepository.cs
+++ ExerciceJwtApiPizza/Repositories/IRepository.cs
@@ -4,18 +4,14 @@ namespace ExerciceJwtApiPizza.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-
         Task<TEntity?> Add(TEntity entity);
 
+        Task<TEntity?> Get(int id); 
+        Task<TEntity?> Get(Expression<Func<TEntity, bool>> predicate); 
+        Task<IEnumerable<TEntity>> GetAll(); 
+        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate); // avec un délégué/fonction pour filtrer
 
-        Task<TEntity?> Get(int id);
-        Task<TEntity?> Get(Expression<Func<TEntity, bool>> predicate);
-        Task<IEnumerable<TEntity>> GetAll();
-        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate);
-
-
-        Task<TEntity?> Update(TEntity entity);
-
+        Task<TEntity?> Update(TEntity entity); 
 
         Task<bool> Delete(int id);
 
