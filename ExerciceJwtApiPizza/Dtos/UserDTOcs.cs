3eb886704c9c90a00ca4fa5a2217bb42ca98a8a6
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-11 23:12:41 +0100

    feat: add UserDTO

---
+++ ExerciceJwtApiPizza/Dtos/UserDTOcs.cs
@@ -0,0 +1,29 @@
+﻿using ExerciceJwtApiPizza.Validator;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExerciceJwtApiPizza.Dtos
+{
+    public class UserDTO
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("firstname")]
+        [Required]
+        public string? FirstName { get; set; }
+        [Column("lastname")]
+        [Required]
+        public string? LastName { get; set; }
+        [Column("is_admin")]
+        [Required]
+        public bool IsAdmin { get; set; } = false;
+        [RegularExpression(@"^([a-zA-Z0-9\.\-_]+)@([a-zA-Z0-9\-_]+)(\.)?([a-zA-Z0-9\-_]+)?(\.){1}([a-zA-Z]{2,11})$", ErrorMessage = "Invalid email address")]
+        [Column("email")]
+        [Required]
+        public String Email { get; set; }
+        [PasswordValidator]
+        [Column("password")]
+        [Required]
+        public String Password { get; set; }
+    }
+}
