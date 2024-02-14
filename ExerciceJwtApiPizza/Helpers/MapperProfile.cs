f74be9a5874a8c4d98f5f1b4c6cefe19e4452ae6
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-14 09:37:21 +0100

    feat: update Mapper Profile

---
+++ ExerciceJwtApiPizza/Helpers/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ExerciceJwtApiPizza.Dtos;
+using ExerciceJwtApiPizza.Models;
+
+namespace ExerciceJwtApiPizza.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+
+            CreateMap<Ingredient, IngredientDTO>().ReverseMap();
+
+            CreateMap<Pizza, PizzaDTO>().ReverseMap();
+
+        }
+    }
+}
