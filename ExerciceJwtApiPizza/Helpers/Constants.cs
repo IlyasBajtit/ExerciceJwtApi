4207318e18d8ef539fae397da4bd34160c56aeeb
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-14 09:37:07 +0100

    feat: update Constant

---
+++ ExerciceJwtApiPizza/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciceJwtApiPizza.Helpers
+{
+    public class Constants
+    {
+        public const string PolicyAdmin = "PolicyAdmin";
+        public const string PolicyUser = "PolicyUser";
+        public const string RoleAdmin = "Admin";
+        public const string RoleUser = "User";
+
+    }
+}
