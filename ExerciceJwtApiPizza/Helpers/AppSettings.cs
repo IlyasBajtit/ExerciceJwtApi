367980b2a531657a5ffeb6c1da8f5494fc92c0f8
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-14 09:36:45 +0100

    feat: update AppSettings

---
+++ ExerciceJwtApiPizza/Helpers/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciceJwtApiPizza.Helpers
+{
+    public class AppSettings
+    {
+
+        public string? SecretKey { get; set; }
+        public string? ValidAudience { get; set; }
+        public string? ValidIssuer { get; set; }
+
+
+
+    }
+}
